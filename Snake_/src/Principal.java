90f2a4bebd6168b9ee72e70775a17211e152c82e
Diego <diegovillaveza@gmail.com>
2016-02-27 00:45:06 +0100

    Menu de prueba

--- Snake_/src/Principal.java
+++ Snake_/src/Principal.java
@@ -2,6 +2,12 @@
 public class Principal {
 
 	public static void main(String [] Args){
-		System.out.println("Hola Mundo");
+		System.out.println("Bienvenidos al Snake:");
+		int op=0;
+		do{
+			System.out.println("1.-Introducir nombre del jugador");
+			System.out.println("2.-Jugar nueva partida");
+			
+		}while(op!=0);
 	}
 }
